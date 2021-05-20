f890579cc1ed1958f9a8e3496797fd84ef372cc2
Edson Cardoso <edsonlopescardoso21@gmail.com>
2021-05-20 10:11:31 +0100

    project

--- src/connectfour/gui/Board.java
+++ src/connectfour/gui/Board.java
@@ -1,19 +1,21 @@
 package connectfour.gui;
 
+import connectfour.View;
 import connectfour.model.Cell;
 import connectfour.model.ConnectFourModel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.layout.GridPane;
 
-public class Board extends GridPane {
+public class Board extends GridPane implements View {
 
-    private int line = 6;
-    private int col = 7;
     private CellButton[][] buttons;
+    private final ConnectFourModel connectFourModel;
+    //private CellButton cellButton;
 
     // constructor class
     public Board(){
+        this.connectFourModel = new ConnectFourModel(this);
         this.createBoard();
     }
 
@@ -23,10 +25,10 @@ public class Board extends GridPane {
        ConnectFourButtonHandler buttonHandler = new ConnectFourButtonHandler();
        // this.buttons = new CellButton[line][col];
 
-        this.buttons = new CellButton[this.line][this.col];
-        for (int i = 0; i < line ; i++) {
-            for (int j = 0; j < col; j++) {
-                CellButton btn = new CellButton(); // create object of CellButton class
+        this.buttons = new CellButton[this.connectFourModel.LINE][this.connectFourModel.COL];
+        for (int i = 0; i < this.connectFourModel.LINE ; i++) {
+            for (int j = 0; j < this.connectFourModel.COL; j++) {
+                CellButton btn= new CellButton(); // create object of CellButton class
                 btn.setOnAction(buttonHandler);
                 this.add(btn, j, i);
                 this.buttons[i][j] = btn;
@@ -34,21 +36,26 @@ public class Board extends GridPane {
         }
     }
 
+    @Override
+    public void update(Cell cell, int line, int col) {
+        CellButton button = buttons[line][col];
+        if(cell == Cell.PLAYER1){
+            button.setTic();
+        }else {
+            button.setTac();
+        }
+    }
+
     class ConnectFourButtonHandler implements EventHandler<ActionEvent>{
-        int counter = 0;
+        //int counter = 0;
         @Override
         public void handle(ActionEvent event) {
             CellButton button = (CellButton) event.getSource();
-            if(counter % 2 == 0){ // if it's even
-                button.setTic();
-            }else { // if it is odd
-                button.setTac();
-            }
-            counter++;
+            int line = Board.this.getRowIndex(button);
+            int col = Board.this.getColumnIndex(button);
 
-
-            //int line = Board.this.getRowIndex(button);
-           // int col = Board.this.getColumnIndex(button);
+            System.out.println(line + " " + col);
+            connectFourModel.cellSelected(col);
         }
     }
 }
--- src/connectfour/model/ConnectFourModel.java
+++ src/connectfour/model/ConnectFourModel.java
@@ -1,10 +1,28 @@
 package connectfour.model;
 
-public class ConnectFourModel {
+import connectfour.View;
 
-    final int LINE = 6;
-    final int COL = 7;
+public class ConnectFourModel{
+
+    private final View VIEW;
+    public final int LINE = 6;
+    public final int COL = 7;
     Cell[][] boardData;
+    int turnCounter = 0;
+
+    public ConnectFourModel(View view){
+        this.VIEW = view;
+        this.boardData = new Cell[this.LINE][this.COL];
+        this.fillBoard();
+    }
+
+    private void fillBoard() {
+        for (int line = 0; line < this.LINE; line++) {
+            for (int col = 0; col < this.COL; col++) {
+                this.boardData[line][col] = Cell.EMPTY;
+            }
+        }
+    }
 
     private boolean isFree(int line, int col) {
         if (boardData[line][col] == Cell.EMPTY){
@@ -13,6 +31,33 @@ public class ConnectFourModel {
         return false;
     }
 
+    public int cellSelected(int col){
+        int line = this.startFromBottom(col);
+        this.updateSelected(line, col);
+
+        return line;
+    }
+
+    private void updateSelected(int line, int col){
+        if (isFree(line, col)){
+            this.updateBoardState(line, col);
+            Cell cell = getCell(line, col);
+            VIEW.update(cell, line, col);
+            turnCounter++;
+        }
+    }
+
+    private Cell getCell(int line, int col){
+        return this.boardData[line][col];
+    }
+
+    private void updateBoardState(int line, int col){
+        if (this.turnCounter % 2 == 0){
+            this.boardData[line][col] = Cell.PLAYER1;
+        }else {
+            this.boardData[line][col] = Cell.PLAYER2;
+        }
+    }
     private int startFromBottom (int col){
         for (int line = (LINE - 1); line >= 0; line--) {
             if(isFree(line, col)){
